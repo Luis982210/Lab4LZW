cde69774e0a25e3a939c0c244f1cc10a91d3dabc
Luis982210 <46784371+Luis982210@users.noreply.github.com>
2020-03-22 16:06:20 -0600

    descompresion y nombre archivo

--- Lab3/Controllers/LZWController.cs
+++ Lab3/Controllers/LZWController.cs
@@ -1,25 +1,24 @@
 ﻿using System;
-using System.IO;
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
+using Microsoft.AspNetCore.Http;
 using Microsoft.AspNetCore.Mvc;
+using System.IO;
 using System.ComponentModel;
 using System.Data;
 using System.Drawing;
 using System.Text;
 using System.Diagnostics;
 using Lab3.Clases;
-
 namespace Lab3.Controllers
 {
     [Route("api/[controller]")]
     [ApiController]
-    public class HuffmanController : ControllerBase
+    public class LZWController : ControllerBase
     {
         readonly IHistoryManager History;
-        //constructer
-        public HuffmanController(IHistoryManager History)
+        public LZWController(IHistoryManager History)
         {
             this.History = History;
         }
@@ -29,61 +28,55 @@ namespace Lab3.Controllers
         {
             return new string[] { "value1", "value2" };
         }
+        //----------------------------------------------------------LZW-------------------------------------------------
 
-        //localhost:51626/api/Values/GetWithParam/?nombre=""
-        //[HttpGet("GetWithParam", Name = "Get")]        
-        
-        // POST api/values
-        [HttpPost("Compress",Name= "PostCompressHuffman")]
+
+        [HttpPost("Compress", Name = "PostCompressLZW")]
         public void Post(string RServerPath, string newname)
         {
-            //var UploadedFile = Request.Form.Files[0];
             string WServerPath = "";
             if (RServerPath != "")
             {
-                HuffmanCoder hf = new HuffmanCoder();
+                /*All the pre-compress path preparation*/
+                LZWCompressor lz = new LZWCompressor();
                 string ServerDirectory = Directory.GetCurrentDirectory();
                 string path = Path.Combine(ServerDirectory, "Compress/");
-                if (!Directory.Exists(path)) 
+                if (!Directory.Exists(path))
                     Directory.CreateDirectory(path);
-                WServerPath = path + newname + ".huff";
-                //FileStream ServerFile = new FileStream(RServerPath, FileMode.Create, FileAccess.ReadWrite);
-                //UploadedFile.CopyTo(ServerFile);
-                //ServerFile.Close();
-                hf.Compress(RServerPath, WServerPath);
-                string HistLine = hf.GetFilesMetrics(Path.GetFileName(RServerPath), RServerPath, WServerPath);
+                WServerPath = path + newname + ".LZW";
+                /*Compress the file*/
+                lz.Compress(RServerPath, WServerPath);
+
+                /*adds the compression to the history*/
+                string HistLine = lz.GetFilesMetrics(Path.GetFileName(RServerPath), RServerPath, WServerPath);
                 History.AddToHistory(HistLine);
                 this.WriteOnHistory(HistLine);
             }
         }
-        // POST api/values
-        [HttpPost("Decompress", Name = "PostDecompressHufman")]
+        [HttpPost("Decompress", Name = "PostDecompressLWZ")]
         public void Post(string RServerPath)
         {
-            //var UploadedFile = Request.Form.Files[0];
             string WServerPath = "";
             if (RServerPath != "")
             {
-                HuffmanCoder hf = new HuffmanCoder();
+                /*All the pre-compress path preparation*/
+                LZWCompressor lz = new LZWCompressor();
                 string ServerDirectory = Directory.GetCurrentDirectory();
                 string path = Path.Combine(ServerDirectory, "Decompress/");
                 if (!Directory.Exists(path))
                     Directory.CreateDirectory(path);
-                
                 WServerPath = path + Path.GetFileName(RServerPath);
-                //FileStream ServerFile = new FileStream(RServerPath, FileMode.Create, FileAccess.ReadWrite);
-                //UploadedFile.CopyTo(ServerFile);
-                //ServerFile.Close();
-                string file = hf.uncompress(RServerPath);
-                using (FileStream FileToWrite = new FileStream(WServerPath, FileMode.CreateNew))
-                {
-                    FileToWrite.Write(Encoding.Default.GetBytes(file));
-                }
-                string HistLine = hf.GetFilesMetrics(Path.GetFileName(RServerPath), RServerPath, WServerPath);
+                /*Compress the file*/
+                lz.Decompress(RServerPath, WServerPath);
+
+                /*adds the compression to the history*/
+                string HistLine = lz.GetFilesMetrics(Path.GetFileName(RServerPath), RServerPath, WServerPath);
                 History.AddToHistory(HistLine);
                 this.WriteOnHistory(HistLine);
             }
         }
+
+        //----------------------------------------------------------Extras-------------------------------------------------
         private void WriteOnHistory(string NewLine)
         {
             string HistoryPath = Path.Combine(Directory.GetCurrentDirectory() + "/History.txt");
@@ -96,6 +89,8 @@ namespace Lab3.Controllers
             {
                 Sw.Write(NewLine);
             }
-        }
+
+
+        }        
     }
 }
